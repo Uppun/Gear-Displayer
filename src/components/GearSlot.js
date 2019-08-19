896bd0b0c9cb514b086aeca2c7af99ea5b851373
Uppun <TheSuperBro@gmail.com>
2019-08-19 07:45:23 -0400

    react-spring and some css fixies

--- src/components/GearSlot.js
+++ src/components/GearSlot.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../assets/gearcard.css'
+import '../assets/gearcard.css';
+import {useSpring, animated} from 'react-spring';
 
 const GearSlot = ({item, icon, onClick, slot}) => {
     let iconNumber = '000000' + icon;
@@ -7,8 +8,15 @@ const GearSlot = ({item, icon, onClick, slot}) => {
     const iconSection = iconNumber.replace(/\d{3}$/, '000');
     const iconUrl = `https://xivapi.com/i/${iconSection}/${iconNumber}.png`;
 
+    const [props, set] = useSpring(() => ({boxShadow: '0px 0px 0px black', transform: 'translate(0px, 0px)'}));
+
     return (
-        <div className='gear-card' onClick={() => onClick(slot)}>
+        <animated.div 
+            className='gear-card' 
+            onClick={() => onClick(slot)}
+            onMouseEnter={() => set({boxShadow: '4px 4px 4px black', transform: 'translate(-4px, -4px)'})}
+            onMouseLeave={() => set({boxShadow: '0px 0px 0px black', transform: 'translate(0px, 0px)'})}
+            style={props}>
             <div className='icon-and-name'>
                 <img 
                     src={iconUrl}
@@ -19,7 +27,7 @@ const GearSlot = ({item, icon, onClick, slot}) => {
                     {item.name.en}
                 </div>
             </div>
-        </div>
+        </animated.div>
     )
 
 }
