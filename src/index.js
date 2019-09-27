84c561c2c7d2ce5cde4641bad55d5c61932143f9
Uppun <TheSuperBro@gmail.com>
2019-09-26 20:08:10 -0400

    update for hookrouter memes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom';
+import {setBasepath} from "hookrouter";
 
-ReactDOM.render((
-    <HashRouter>
-        <App />
-    </HashRouter>
-), document.getElementById('root'));
+setBasepath('/Gear-Displayer');
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
